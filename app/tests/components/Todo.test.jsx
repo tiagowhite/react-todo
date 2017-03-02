7422fd84624ba5470a6bf80dae5aaa8a1aec2da4
Tiago Fernandes <tiagowhite@gmail.com>
2017-03-02 06:51:50 -0300

    Adding List Test

---
+++ app/tests/components/Todo.test.jsx
@@ -0,0 +1,14 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const expect = require('expect');
+const $ = require('jQuery');
+const TestUtils = require('react-addons-test-utils');
+
+const Todo = require('Todo');
+
+
+describe('Todo', () => {
+	it('Should exists', () => {
+		expect(Todo).toExist();
+	});
+});
---
+++ app/tests/components/TodoList.test.jsx
@@ -0,0 +1,30 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const expect = require('expect');
+const $ = require('jQuery');
+const TestUtils = require('react-addons-test-utils');
+
+const TodoList = require('TodoList');
+const Todo = require('Todo');
+
+describe('TodoList', () => {
+	it('Should exists', () => {
+		expect(TodoList).toExist();
+	});
+
+	it('should render one Todo component for each todo item', () => {
+		var todos = [{
+			id: 1,
+			text: 'Do something'
+		},{
+			id: 2,
+			text: 'Comprar Lampadas'
+		}];
+
+		var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos} />);
+		var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+
+		expect(todosComponents.length).toBe(todos.length);
+
+	});
+});
