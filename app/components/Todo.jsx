85295ec57214116bc5be1999d3bc53743b3db7ac
Tiago Fernandes <tiagowhite@gmail.com>
2017-03-12 19:38:24 -0300

    Added completed and created at

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,15 +1,32 @@
 const React = require('react');
+const moment = require('moment');
+
 
 var Todo = React.createClass({
+
 	render: function () {
-		var { id, text, completed } = this.props;
+
+		var { id, text, completed, createdAt, completedAt } = this.props;
+
+		var renderDate = () => {
+			var message = 'Created ';
+			var timestamp = createdAt;
+
+			if (completed) {
+				message = 'Completed ';
+				timestamp = completedAt;
+			}
+
+			return message + moment.unix(timestamp).format('MMM Do YYYY @ h:mm a');
+		}
+
 		return (
 			<div onClick={() => {
 					this.props.onToggle(id);
 				}}>
 				<input type="checkbox" checked={completed} readOnly/>
-				<span className="text-center">{text}</span>
-
+				<p>{text}</p>
+				<p>{renderDate()}</p>
 			</div>
 		)
 	}
